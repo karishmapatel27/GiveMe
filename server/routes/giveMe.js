24551f71a6591c18c20b6ae5de2276d9cd577610
benmitchell11 <ben.mitchell11@hotmail.co.nz>
2020-09-24 11:54:59 +1200

    quickfix

---
+++ server/routes/giveMe.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const db = require('../db')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  db.getGardens()
+    .then(getGiveMe => {
+      res.json({getGiveMe})
+    })
+    .catch(err => {
+      res.status(500).json({error: err.message})
+    })
+})
+
+module.exports = router
