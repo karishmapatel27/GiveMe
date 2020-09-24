ba235a4b4fac5abfc1caacbe699773a3c140d428
rafaelnhster <rafaelnhster@gmail.com>
2020-09-24 16:25:18 +1200

    setting up home component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+
 import Register from './Register'
+import Nav from './Nav'
 
 import LandingPage from './Register'
+import SignIn from './SignIn'
 
 function App () {
 
   return (
     <>
+    
       <Route exact path='/' component={ Register } />
+      <Route exact path='/signin' component={ SignIn } />
     </>
   )
 }
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+
+function Nav (){
+return(
+    <>
+    <h1>
+        Nav Bar
+    </h1>
     
+    </>
+)
+
+}
+
+export default Nav
