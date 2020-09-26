eff836cce62c29b6c166da4132b3c6293ceb85ef
Kahu <kahu.leary@outlook.com>
2020-09-26 17:58:37 +1200

    Update to add new item listing

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -8,7 +8,7 @@ function Register (props) {
   const [, setUser] = useContext(UserContext)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [email, setEmail] = useState('')
+  const [email, setUserEmail] = useState('')
   const baseUrl = '/api/v1'
 
   const handleClick = () => {
@@ -33,31 +33,30 @@ function Register (props) {
   }
 
   return (
-    <div className="gradient-background">
-      <div className="wrapper">
-        <div className="form" >
-          <div className='formLogo'>
-            <img className="is-rounded" src='./img/logo-txt-orange.png' />
-          </div>
-          <div className="formRight">
+    // <div className="gradient-background">
+    <div className="wrapper">
+      <div className="form" >
+        <div className='formLeft'>
+          <img className="is-rounded1" src='./img/logo-txt-orange.png' />
+        </div>
+        <div className="formRight">
 
-            <h1>Register</h1>
+          <h1>Register</h1>
 
-            <label className="label">email</label>
-            <input className="input1" type="email" onChange={(e) => setUserEmail(e.target.value)}></input>
+          <input className="input1" placeholder="Email" type="email" onChange={(e) => setUserEmail(e.target.value)}></input>
 
-            <label className="label">Username</label>
-            <input className="input1" type="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
+          <input className="input1" placeholder="Username" type="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
 
-            <label className="label">Password</label>
-            <input className="input1" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-            <button type="button" className="button primaryBtn" onClick={handleClick} data-testid="submitButton">Register</button>
-            <Link className="" onClick={handleClick} to="/signin">Sign In</Link>
+          <input className="input1" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
 
-          </div>
+          <button type="button" className="button primaryBtn" onClick={handleClick} data-testid="submitButton">Register</button>
+
+          <Link className="" onClick={handleClick} to="/signin">Sign In</Link>
+
         </div>
       </div>
     </div>
+    // </div>
   )
 }
 
