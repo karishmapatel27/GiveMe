1bb173f47be071172624677e1f0dd86cc426bf35
Kahu <kahu.leary@outlook.com>
2020-09-27 16:31:05 +1300

    Update item details

--- client/components/ItemDetails.jsx
+++ client/components/ItemDetails.jsx
@@ -11,8 +11,10 @@ function ItemDetails () {
           <p className='itemInfo'>
             GiveMe is an online platform where kiwis can donate their unwanted items to other kiwis, empowering the NZ community to improve sustainability and reduce the waste caused by unwanted products.
           </p>
-          <button className="button primaryBtn">Give Me</button>
-          <button className="button primaryBtn">Watch</button>
+          <div className='btnContainer'>
+            <button className="button primaryBtn btn">Give Me</button>
+            <button className="button primaryBtn">Watch</button>
+          </div>
         </div>
       </div>
     </>
