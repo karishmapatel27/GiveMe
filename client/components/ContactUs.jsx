f6f9f80513c05fd44a1e62ed6502a49e8b818485
Kahu <kahu.leary@outlook.com>
2020-09-28 10:14:00 +1300

    Style email form

--- client/components/ContactUs.jsx
+++ client/components/ContactUs.jsx
@@ -15,28 +15,28 @@ export default function ContactUs () {
   }
 
   return (
-    <div>
-      <div className="container">
-        <form onSubmit={sendEmail}>
-          <div className="row pt-5 mx-auto">
-            <div className="col-8 form-group mx-auto">
-              <input type="text" className="form-control" placeholder="Name" name="name" />
-            </div>
-            <div className="col-8 form-group pt-2 mx-auto">
-              <input type="email" className="form-control" placeholder="Email Address" name="email" />
-            </div>
-            <div className="col-8 form-group pt-2 mx-auto">
-              <input type="text" className="form-control" placeholder="Subject" name="subject" />
-            </div>
-            <div className="col-8 form-group pt-2 mx-auto">
-              <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-            </div>
-            <div className="col-8 pt-3 mx-auto">
-              <input type="submit" className="btn btn-info" value="Send Message"></input>
-            </div>
-          </div>
-        </form>
+    <>
+      <div className="form">
+        <div className="formLeft">
+          <img src="./img/logo-txt-orange.png" alt="logo" className="logoImg"/>
+        </div>
+        <div className="formRight">
+          <h1>Contact Us</h1>
+          <form onSubmit={sendEmail}>
+
+            <input type="text" className="formInput" placeholder="Name" name="name" />
+
+            <input type="email" className="formInput" placeholder="Email Address" name="email" />
+
+            <input type="text" className="formInput" placeholder="Subject" name="subject" />
+
+            <textarea className="textarea" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+
+            <input type="submit" className="button primaryBtn" value="Send Message"></input>
+          </form>
+        </div>
+
       </div>
-    </div>
+    </>
   )
 }
