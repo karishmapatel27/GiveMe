ea26c344634a4fe0dc8dd14aa2f175e26b0749e0
Kahu <kahu.leary@outlook.com>
2020-09-28 07:19:20 +1300

    Add Link import in Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { isAuthenticated } from 'authenticare/client'
 import { UserContext, updateUserContext } from './UserContext'
 
