3dedd3fc4ee8e9f470c72ac8dbec5155a92efe2e
Kahu <kahu.leary@outlook.com>
2020-09-29 14:17:58 +1300

    Update footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+// import { fa }
 
 export default function Footer () {
   return (
     <div className="footer">
       <footer>
-        <p>&copy; 2020 GiveMe. All rights reserved</p>
-        <a href="#" className="fa fa-facebook"></a>
-        <a href="#" className="fa fa-twitter"></a>
-        <a href="#" className="fa fa-google"></a>
-        <a href="https://github.com/pohutukawa-2020/GiveMe" target="_blank" rel='noreferrer' className="fa fa-github"></a>
+        <div className='footer-left'>
+          <p>&copy; 2020 GiveMe. All rights reserved</p>
+        </div>
+        <div className='socialMedia'>
+          <a href="#" className="fa fa-facebook"></a>
+          <a href="#" className="fa fa-twitter"></a>
+          <a href="#" className="fa fa-google"></a>
+          <a href="https://github.com/pohutukawa-2020/GiveMe" target="_blank" rel='noreferrer' className="fa fa-github"></a>
+        </div>
       </footer>
     </div>
   )
--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import { HashRouter as Router } from 'react-router-dom'
 import { UserProvider } from './components/UserContext'
 
 import '../server/public/style/main.scss'
+// import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 import App from './components/App'
 
