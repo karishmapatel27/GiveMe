3ba95efa53f7de69b85627c169112922a0a5fb43
Karishma <karishmapatel93@hotmail.com>
2020-10-01 12:28:00 +1300

    clean database seeds

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -12,11 +12,11 @@ exports.seed = function (knex) {
     ]))
     .then(([adminHash, benHash, karishmaHash, kahuHash, rafaelHash]) =>
       knex('users').insert([
-        { id: 1, username: 'admin', email: 'admin@admin.com', hash: adminHash, isAdmin: true },
-        { id: 2, username: 'ben', email: 'ben@test.com', hash: benHash, isAdmin: false },
-        { id: 3, username: 'karishma', email: 'karishma@test.com', hash: karishmaHash, isAdmin: false },
-        { id: 4, username: 'kahu', email: 'kahu@test.com', hash: kahuHash, isAdmin: false },
-        { id: 5, username: 'rafael', email: 'rafael@test.com', hash: rafaelHash, isAdmin: false }
+        { id: 1, username: 'joe', email: 'joe22@gmail.com', hash: adminHash, isAdmin: true },
+        { id: 2, username: 'ben', email: 'ben@gmail.com', hash: benHash, isAdmin: false },
+        { id: 3, username: 'karishma', email: 'karishmapatel@hotmail.com', hash: karishmaHash, isAdmin: false },
+        { id: 4, username: 'kahu', email: 'kahu@outlook.com', hash: kahuHash, isAdmin: false },
+        { id: 5, username: 'rafael', email: 'rafael@gmail.com', hash: rafaelHash, isAdmin: false }
       ])
     )
 }
